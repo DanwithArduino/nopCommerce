c7aca181401f46189bd33b5128799e5c925f9422
daniel goldman <dg.danielgoldman@gmail.com>
2024-11-01 19:10:27 -0400

    i can see the changes.

--- src/Plugins/Nop.Plugin.Integration.SageIntegration/SageIntegrationPlugin.cs
+++ src/Plugins/Nop.Plugin.Integration.SageIntegration/SageIntegrationPlugin.cs
@@ -1,4 +1,11 @@
-﻿using Nop.Core.Domain.Orders;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Orders;
+using Nop.Core.Events;
+using Nop.Core.Infrastructure;
 using Nop.Services.Events;
 using Nop.Services.Plugins;
 
@@ -6,6 +13,7 @@ namespace Nop.Plugin.Misc.SageIntegration;
 
 public class SageIntegrationPlugin : BasePlugin
 {
+
     public override async Task InstallAsync()
     {
         //Logic during installation goes here...
@@ -30,4 +38,38 @@ public class SageIntegrationPlugin : BasePlugin
             }
         }
     }
+
+}
+
+public class EventConsumer2 : IConsumer<CustomerLoggedinEvent>
+{
+    public async Task HandleEventAsync(CustomerLoggedinEvent eventMessage)
+    {
+        if (eventMessage?.Customer != null)
+        {
+            Debug.WriteLine("im here");
+            //do something
+        }
+    }
+}
+
+public class NopStartup : INopStartup
+{
+    public virtual void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        //register UPSService
+
+    }
+
+    /// <summary>
+    /// Configure the using of added middleware
+    /// </summary>
+    /// <param name="application">Builder for configuring an application's request pipeline</param>
+    public void Configure(IApplicationBuilder application)
+    {
+    }
+    /// <summary>
+    /// Gets order of this startup configuration implementation
+    /// </summary>
+    public int Order => 3000;
 }
